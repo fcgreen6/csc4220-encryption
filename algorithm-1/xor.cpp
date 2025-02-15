5cb5dc1bd8dfb132bb69f434881aa0c2b9a4bf4f
fcgreen6 <122060950+fcgreen6@users.noreply.github.com>
2025-02-14 16:27:54 -0800

    File Management
    Completed the file management system. To operate on a user specified file, use the ifstream provided. To save encrypted or decrypted files,, use the ofstream provided.

--- algorithm-1/xor.cpp
+++ algorithm-1/xor.cpp
@@ -9,6 +9,8 @@ using std::cerr;
 using std::ifstream;
 using std::ofstream;
 
+string GetFileName(string filePath);
+
 int main(int argc, char* argv[]) {
 
     string userInput;
@@ -17,6 +19,7 @@ int main(int argc, char* argv[]) {
     cin >> userInput;
     cout << "\n";
 
+    // Try to collect user input again if command is not recognised.
     while ((userInput != "1") && (userInput != "2") && (userInput != "3")) {
 
         cerr << "Error: Command not recognised. Please try again.\n";
@@ -29,58 +32,89 @@ int main(int argc, char* argv[]) {
 
         if (userInput == "1") {
 
-            cout << "Enter path to specified file.\n";
+            cout << "Enter path to specified file:\n";
 
-            // TODO: Remove this print statement (debug).
-            cout << "(Encryption)\n";
-            
             cin >> userInput;
             cout << "\n";
-            
+
+            // Attempt to open specified file.
             ifstream encryptionFS;
             encryptionFS.open(userInput);
 
             while (!encryptionFS.is_open()) {
 
+                // Reset state flags.
+                encryptionFS.clear();
+
                 cerr << "Error: File not recognised. Please try again.\n";
                 cout << "Enter path to specified file:\n";
 
                 cin >> userInput;
                 cout << "\n";
-                //TODO: Encryption code here (maybe make a new folder and create new encrypted files rather than editing existing files)
-                encryptionFS.close();
+                
+                // Attempt to reopen the file.
                 encryptionFS.open(userInput);
             }
+
+            // TODO: Encryption code here...
+
+            // Get the name of the file specified by the user. Create new file name for saving the decrypted product.
+            string outFileName = GetFileName(userInput);
+            outFileName = "./encrypted/" + outFileName + "-encrypted.txt";
+
+            // Put decrypted product into this file stream to save in within the decryption folder.
+            ofstream outputFS;
+            outputFS.open(outFileName, std::ios::trunc); // Trunc trait removes any existing elements within the file.
+
+            // Close file streams.
+            encryptionFS.close();
+            outputFS.close();
         } else {
 
-            cout << "Enter path to specified file.\n";
-
-            // TODO: Remove this print statement (debug).
-            cout << "(Decryption)\n";
+            cout << "Enter path to specified file:\n";
 
             cin >> userInput;
             cout << "\n";
 
+            // Attempt to open specified file.
             ifstream decryptionFS;
             decryptionFS.open(userInput);
 
             while (!decryptionFS.is_open()) {
 
+                // Reset state flags.
+                decryptionFS.clear();
+
                 cerr << "Error: File not recognised. Please try again.\n";
                 cout << "Enter path to specified file:\n";
 
                 cin >> userInput;
                 cout << "\n";
-                //TODO: Decryption code here (maybe make a new folder and create new decrypted files rather than editing existing files)
-                decryptionFS.close();
+                
+                // Attempt to reopen the file.
                 decryptionFS.open(userInput);
             }
+
+            // TODO: Decryption code here...
+
+            // Get the name of the file specified by the user. Create new file name for saving the decrypted product.
+            string outFileName = GetFileName(userInput);
+            outFileName = "./decrypted/" + outFileName + "-decrypted.txt";
+
+            // Put decrypted product into this file stream to save in within the decryption folder.
+            ofstream outputFS;
+            outputFS.open(outFileName, std::ios::trunc); // Trunc trait removes any existing elements within the file.
+
+            // Close file streams.
+            decryptionFS.close();
+            outputFS.close();
         }
 
         cout << "Input \"1\" to encrypt a file. Input \"2\" to decrypt a file. Input \"3\" to quit the program.\n";
         cin >> userInput;
         cout << "\n";
 
+        // Try to collect user input again if command is not recognised.
         while ((userInput != "1") && (userInput != "2") && (userInput != "3")) {
 
             cerr << "Error: Command not recognised. Please try again.\n";
@@ -89,4 +123,24 @@ int main(int argc, char* argv[]) {
             cout << "\n";
         }
     }
+}
+
+// GetFileName Function:
+// - Purpose: Given a file path, return the name of the specified file ignoring the extension.
+// - Parameters:
+//   - filePath: A string containing the file path to the specified file.
+// - Return: The name of the specified file. Example: Given filePath "./test.txt" the function should return "test".
+string GetFileName(string filePath) {
+
+    // Find the postions of the final backslash and period of the file path.
+    size_t separatorOne = filePath.find_last_of("/");
+    size_t separatorTwo = filePath.find_last_of(".");
+
+    // If there is no backslask due to the file being relative, start the substring at index zero.
+    if (separatorOne == string::npos) {
+
+        return filePath.substr(0, separatorTwo);
+    }
+
+    return filePath.substr(separatorOne + 1, separatorTwo - separatorOne - 1);
 }

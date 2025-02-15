5cf4e8c154eccbf8ff8a49bb345d0734320718c7
chana8spu <121988455+chana8spu@users.noreply.github.com>
2025-02-15 12:19:02 -0800

    Shift encryption/decryption

--- algorithm-2/shift.cpp
+++ algorithm-2/shift.cpp
@@ -2,145 +2,144 @@
 #include <iostream>
 #include <fstream>
 
-using std::string;
+using std::cerr;
 using std::cin;
 using std::cout;
-using std::cerr;
 using std::ifstream;
 using std::ofstream;
+using std::string;
 
-string GetFileName(string filePath);
+// Shift encryption function
+void shiftEncrypt(const string &inputFile, const string &outputFile, int key) {
+    ifstream inFile(inputFile, std::ios::binary);
+    ofstream outFile(outputFile, std::ios::binary);
+    char ch;
+    // XOR each character in the file with the key
+    while (inFile.get(ch))
+    {
+        outFile.put(static_cast<char>(ch + key));
+    }
 
-int main(int argc, char* argv[]) {
+    inFile.close();
+    outFile.close();
 
-    string userInput;
+    cout << "Operation completed! Output saved to: " << outputFile << "\n\n";
+}
 
+// Shift decryption function
+void shiftDecrypt(const string &inputFile, const string &outputFile, int key) {
+    ifstream inFile(inputFile, std::ios::binary);
+    ofstream outFile(outputFile, std::ios::binary);
+    char ch;
+    // XOR each character in the file with the key
+    while (inFile.get(ch))
+    {
+        outFile.put(static_cast<char>(ch - key));
+    }
+
+    inFile.close();
+    outFile.close();
+
+    cout << "Operation completed! Output saved to: " << outputFile << "\n\n";
+}
+
+bool manageFiles(const string &inputFile, const string &outputFile) {
+    ifstream inFile(inputFile, std::ios::binary);
+    if (!inFile) {
+        cerr << "Error: Cannot open file " << inputFile << "\n";
+        return false;
+    }
+
+    ofstream outFile(outputFile, std::ios::binary);
+    if (!outFile) {
+        cerr << "Error: Cannot create file " << outputFile << "\n";
+        return false;
+    }
+    return true;
+}
+// Function to get valid user input (1, 2, or 3)
+string getValidUserChoice()
+{
+    string choice;
     cout << "Input \"1\" to encrypt a file. Input \"2\" to decrypt a file. Input \"3\" to quit the program.\n";
-    cin >> userInput;
+    cin >> choice;
     cout << "\n";
 
     // Try to collect user input again if command is not recognised.
-    while ((userInput != "1") && (userInput != "2") && (userInput != "3")) {
+    while ((choice != "1") && (choice != "2") && (choice != "3")) {
 
         cerr << "Error: Command not recognised. Please try again.\n";
         cout << "Input \"1\" to encrypt a file. Input \"2\" to decrypt a file. Input \"3\" to quit the program.\n";
-        cin >> userInput;
+        cin >> choice;
         cout << "\n";
     }
-
-    while ((userInput == "1") || (userInput == "2")) {
-
-        if (userInput == "1") {
-
-            cout << "Enter path to specified file:\n";
-
-            cin >> userInput;
-            cout << "\n";
-
-            // Attempt to open specified file.
-            ifstream encryptionFS;
-            encryptionFS.open(userInput);
-
-            while (!encryptionFS.is_open()) {
-
-                // Reset state flags.
-                encryptionFS.clear();
-
-                cerr << "Error: File not recognised. Please try again.\n";
-                cout << "Enter path to specified file:\n";
-
-                cin >> userInput;
-                cout << "\n";
-                
-                // Attempt to reopen the file.
-                encryptionFS.open(userInput);
-            }
-
-            // TODO: Encryption code here...
-
-            // Get the name of the file specified by the user. Create new file name for saving the decrypted product.
-            string outFileName = GetFileName(userInput);
-            outFileName = "./encrypted/" + outFileName + "-encrypted.txt";
-
-            // Put decrypted product into this file stream to save in within the decryption folder.
-            ofstream outputFS;
-            outputFS.open(outFileName, std::ios::trunc); // Trunc trait removes any existing elements within the file.
-
-            // Close file streams.
-            encryptionFS.close();
-            outputFS.close();
-        } else {
-
-            cout << "Enter path to specified file:\n";
-
-            cin >> userInput;
-            cout << "\n";
-
-            // Attempt to open specified file.
-            ifstream decryptionFS;
-            decryptionFS.open(userInput);
-
-            while (!decryptionFS.is_open()) {
-
-                // Reset state flags.
-                decryptionFS.clear();
-
-                cerr << "Error: File not recognised. Please try again.\n";
-                cout << "Enter path to specified file:\n";
-
-                cin >> userInput;
-                cout << "\n";
-                
-                // Attempt to reopen the file.
-                decryptionFS.open(userInput);
-            }
-
-            // TODO: Decryption code here...
-
-            // Get the name of the file specified by the user. Create new file name for saving the decrypted product.
-            string outFileName = GetFileName(userInput);
-            outFileName = "./decrypted/" + outFileName + "-decrypted.txt";
-
-            // Put decrypted product into this file stream to save in within the decryption folder.
-            ofstream outputFS;
-            outputFS.open(outFileName, std::ios::trunc); // Trunc trait removes any existing elements within the file.
-
-            // Close file streams.
-            decryptionFS.close();
-            outputFS.close();
-        }
-
-        cout << "Input \"1\" to encrypt a file. Input \"2\" to decrypt a file. Input \"3\" to quit the program.\n";
-        cin >> userInput;
-        cout << "\n";
-
-        // Try to collect user input again if command is not recognised.
-        while ((userInput != "1") && (userInput != "2") && (userInput != "3")) {
-
-            cerr << "Error: Command not recognised. Please try again.\n";
-            cout << "Input \"1\" to encrypt a file. Input \"2\" to decrypt a file. Input \"3\" to quit the program.\n";
-            cin >> userInput;
-            cout << "\n";
-        }
-    }
+    return choice;
 }
 
-// GetFileName Function:
-// - Purpose: Given a file path, return the name of the specified file ignoring the extension.
-// - Parameters:
-//   - filePath: A string containing the file path to the specified file.
-// - Return: The name of the specified file. Example: Given filePath "./test.txt" the function should return "test".
-string GetFileName(string filePath) {
+// Function to extract only the filename from a full path
+string getFilenameFromPath(const string &fullPath)
+{
+    size_t pos = fullPath.find_last_of("\\/");
+    return (pos == string::npos) ? fullPath : fullPath.substr(pos + 1);
+}
 
-    // Find the postions of the final backslash and period of the file path.
-    size_t separatorOne = filePath.find_last_of("/");
-    size_t separatorTwo = filePath.find_last_of(".");
+// Function to extract the directory path from the full path
+string getDirectoryFromPath(const string &fullPath)
+{
+    size_t pos = fullPath.find_last_of("\\/"); // in case of format like C:\path\to\file
+    return (pos == string::npos) ? "" : fullPath.substr(0, pos + 1);
+}
 
-    // If there is no backslask due to the file being relative, start the substring at index zero.
-    if (separatorOne == string::npos) {
+// Function to get a valid file path from the user
+string getValidFilePath()
+{
+    string filePath;
+    ifstream file;
 
-        return filePath.substr(0, separatorTwo);
+    cout << "Enter the path to the specified file:\n";
+    cin >> filePath;
+    cout << "\n";
+
+    file.open(filePath);
+    while (!file.is_open())
+    {
+        cerr << "Error: File not recognised. Please try again.\n";
+        cout << "Enter path to specified file:\n";
+        cin >> filePath;
+        cout << "\n";
+        file.open(filePath);
+    }
+    file.close();
+    return filePath;
+}
+
+int main()
+{
+    int key = 4; // Shift encryption key
+    string userChoice = getValidUserChoice();
+
+    while (userChoice != "3") // Keep running until user chooses to exit
+    {
+        string inputFile = getValidFilePath();
+        string filename = getFilenameFromPath(inputFile);   // Extracts only filename
+        string directory = getDirectoryFromPath(inputFile); // Extracts the directory path
+        string outputFile;
+
+        if (userChoice == "1") // Encryption
+        {
+            outputFile = inputFile + ".enc"; // Save encrypted file with .enc extension
+            shiftEncrypt(inputFile, outputFile, key);
+        }
+        else if (userChoice == "2") // Decryption
+        {
+            outputFile = directory + "decrypted_" + filename; // Save decrypted file correctly
+            shiftDecrypt(inputFile, outputFile, key);
+        }
+
+        // Ask user for next action
+        userChoice = getValidUserChoice();
     }
 
-    return filePath.substr(separatorOne + 1, separatorTwo - separatorOne - 1);
+    cout << "Program exited.\n";
+    return 0;
 }
